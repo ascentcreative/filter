03186b4f081f324da7180add85d13747c86564cf
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-02-17 14:14:05 +0000

    Copy and Export

--- assets/ascent-filter-view.js
+++ assets/ascent-filter-view.js
@@ -3,6 +3,9 @@
 $.ascent = $.ascent?$.ascent:{};
 
 var FilterView = {
+
+    initialState: null,
+    baseUri: '',
         
 		_init: function () {
             
@@ -16,8 +19,41 @@ var FilterView = {
                 self.loadPage(e);
             });
 
+
+            $(this.element).on('request-copydata', function(e, slug, triggerEvent) {
+                self.copyData(e, slug, triggerEvent);
+            });
+
+            $(this.element).on('click', '.filter-export', function() {
+                self.exportData();
+            });
+
             // new page requested. 
+            // alert('loaded');
+
+            // handle loading of data on history navigation:
+            window.onpopstate = function(e) {
+                // alert('going back');
+                console.log(e);
+                // console.log(e.state.data);
+                if(e.state) {
+                    self.setState(e.state);
+                    // $(self.element).html(e.state); // set display data
+                } else {
+                    //$(self.element).html(self.initialState);
+                }
+                // also need to change the filter form data...
+            };
 			
+            this.initialState = {
+               
+            }
+
+            let pathary = $(this.element).attr('action').split('/');
+            let pop = pathary.pop();
+            this.baseUri = (pathary.join('/'));
+
+
             this.element.addClass("initialised");
 			
 		},
@@ -71,7 +107,7 @@ var FilterView = {
             let qs = $(this.element).find("INPUT, SELECT").not('[name=_token]').serialize();
             
             $.ajax({ 
-                url: '/filter/loadpage',
+                url: this.baseUri + "/loadpage",
                 type: 'post',  
                 data: filterData,
                 contentType: false,
@@ -81,23 +117,14 @@ var FilterView = {
                 }
             }).done(function(data) {
 
-                for(const id in data.displays) {
-                    $(self.element).find('.filter-display#' + id).html(data.displays[id]);
-                };
-
-                for(const id in data.paginators) {
-                    $(self.element).find('.filter-paginator#' + id).html(data.paginators[id]);
-                }
-
-                for(const id in data.counters) {
-                    $(self.element).find('.filter-counter#' + id).html(data.counters[id]);
-                }
+                self.setState(data);
 
                 // pushState:
                 // call the History API to update the URL in the browser:
                 // or, maybe this should be done by the display when the first page is loaded, so the results can be stored?
                 let href = window.location.pathname;
-                history.pushState({'data': data}, 'title', href + '?' + qs);
+                console.log('pushing state:', data);
+                history.pushState(data, 'title', href + '?' + qs);
                
 
             }).then(function() {
@@ -107,6 +134,82 @@ var FilterView = {
             // submit a query to the filter route
 
             // update the DOM
+        },
+
+        copyData: function(e, col, triggerEvent) {
+
+            // alert('copying ' + col);
+            // console.log();
+            let self = this;
+
+            $(this.element).css('opacity', 0.2);
+
+            var filterData = new FormData($(this.element)[0]);
+            filterData.append('config', $(this.element).data('filtersetup'));
+
+            // does an AJAX request to get ALL pages of data,not just current.
+            $.ajax({ 
+                url: this.baseUri + '/copy/' + col,
+                type: 'post',  
+                data: filterData,
+                contentType: false,
+                processData: false,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            }).done(function(data) {
+
+                let copyFrom = document.createElement("textarea");
+		        document.body.appendChild(copyFrom);
+		        copyFrom.textContent = data.data;
+		        copyFrom.select();
+		        document.execCommand("copy");
+		        copyFrom.remove();
+
+
+                let toast = $(data.toast);
+                $('body').append(toast);
+
+                let causer = triggerEvent.target;
+                let rect = causer.getBoundingClientRect();
+                let tRect = toast[0].getBoundingClientRect();
+
+                $(toast).on('hidden.bs.toast', function () {
+                    $(toast).remove();
+                })
+                $(toast).css('top', (rect.bottom) + window.scrollY + 'px').css('left', (rect.right - tRect.width) + 'px')
+                        .toast('show');
+
+                $(self.element).css('opacity', 1);
+
+
+            }).fail(function(data) {
+                alert('unable to copy data');
+            });
+
+
+        },
+
+        exportData: function(e, col, triggerEvent) {
+        
+            window.location = this.baseUri + '/export?' + window.location.search;
+
+        },
+
+        setState: function(data) {
+
+            for(const id in data.displays) {
+                $(this.element).find('.filter-display#' + id).html(data.displays[id]);
+            };
+
+            for(const id in data.paginators) {
+                $(this.element).find('.filter-paginator#' + id).html(data.paginators[id]);
+            }
+
+            for(const id in data.counters) {
+                $(this.element).find('.filter-counter#' + id).html(data.counters[id]);
+            }
+
         }
 
        
