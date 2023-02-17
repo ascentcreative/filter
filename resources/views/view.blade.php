741e7f37682552ba666963b2818600eeee533b23
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-02-17 14:14:42 +0000

    URI from FFM

--- resources/views/view.blade.php
+++ resources/views/view.blade.php
@@ -1,8 +1,10 @@
 @props(['idx'])
-<form class="filter-view {{ $attributes['class'] }}" style="{{ $attributes['style'] }}"  
+<form class="filter-view {{ $attributes['class'] }}" style="{{ $attributes['style'] }}" method="post" 
+    action="{{ $filterManager::getInstance()->getRouteUri('loadpage') }}"
     data-filtersetup="{{ Crypt::encryptString(json_encode([
                 'filterManager'=>$filterManager
-            ])) }}" >
+            ])) }}" 
+            >
     @csrf
     {{ $slot}}
 </form>
