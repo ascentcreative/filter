39df0316b44012743dcac77e861f347416046e4a
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-03-21 16:40:52 +0000

    Commented attempt at 'no records' display. Need to fix for tables

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,4 +1,5 @@
 @aware(['filterManager', 'items'])
+{{-- @if($items->total() > 0) --}}
 <{{ $tag }} class="filter-display {{ $attributes['class'] }}" style="flex-basis: 100%; {{ $attributes['style'] }}" id="display-{{ uniqid() }}"
         data-config="{{ Crypt::encryptString(json_encode([
             'itemBlade'=>$attributes['itemBlade']
@@ -10,4 +11,9 @@
             'blade'=>$attributes['itemBlade']])
 
 </{{ $tag }}>
+{{-- @else
+
+<H1 style="padding: 40px; text-align: center; color: #ccc">No Records Found</H1>
+
+@endif --}}
 
