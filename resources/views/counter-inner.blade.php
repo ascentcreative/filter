41e07855947a5c15a335d157d1e56d9798cc1048
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2024-10-10 11:08:09 +0100

    No Matching [xyz]

--- resources/views/counter-inner.blade.php
+++ resources/views/counter-inner.blade.php
@@ -1,3 +1,5 @@
-@if($items)
+@if($items && $items->count() > 0)
     Showing {{ $items->firstItem() }}-{{ $items->lastItem() }} of {{ $items->total() }} {{ Str::plural($attributes['unit'], $items->count()) }}
+@else
+    No matching {{ Str::plural($attributes['unit'], $items->count()) }}
 @endif
